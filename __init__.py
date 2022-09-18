0bca295ec108f9327682b263514ab34ef2867557
oskro <oskro@orsted.com>
2022-09-18 21:07:05 +0200

    add automatic logging initiator to init file

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from .utils.logging_helper import initiate_logger as __initiate_logger
+__initiate_logger()
+
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
