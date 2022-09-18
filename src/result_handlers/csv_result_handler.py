3d5e12ebe956e408598499f7614ae84bb788c7c4
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-09-18 21:46:34 +0200

    Typing.

--- src/result_handlers/csv_result_handler.py
+++ src/result_handlers/csv_result_handler.py
@@ -5,19 +5,19 @@ from ..result_handlers.result_handler import ResultHandler
 
 
 class CsvResultHandler(ResultHandler):
-    def result_exists(self):
+    def result_exists(self) -> bool:
         return os.path.exists(self.csv_path)
 
-    def export_result(self, result: FixedRateBondData):
+    def export_result(self, result: FixedRateBondData) -> None:
         if not os.path.exists(self.day_path):
             os.makedirs(self.day_path)
 
         result.to_pandas_data_frame().to_csv(self.csv_path, index=False)
 
     @property
-    def csv_path(self):
+    def csv_path(self) -> str:
         return f"{self.day_path}/{self.scrape_time.strftime('%Y%m%d%H%M')}.csv"
 
     @property
-    def day_path(self):
+    def day_path(self) -> str:
         return f"./Kurser/{self.scrape_time.strftime('%Y%m%d')}"
--- src/result_handlers/result_handler.py
+++ src/result_handlers/result_handler.py
@@ -5,8 +5,8 @@ class ResultHandler:
     def __init__(self, scrape_time: datetime):
         self.scrape_time = scrape_time
 
-    def result_exists(self):
+    def result_exists(self) -> bool:
         raise NotImplementedError
 
-    def export_result(self, result):
+    def export_result(self, result) -> None:
         raise NotImplementedError
