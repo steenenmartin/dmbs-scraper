bda3bd2e5eaf88859d1fb58fee832a25de0201c5
oskro <oskro@orsted.com>
2022-09-18 20:59:54 +0200

    add client factory to query database

---
+++ database.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sqlite3
+DATABASE_PATH = "database.db"
+
+
+def query_db(sql: str) -> pd.DataFrame:
+    with client_factory() as conn:
+        result = pd.read_sql(sql=sql, con=conn)
+    return result
+
+
+def client_factory():
+    return sqlite3.connect(DATABASE_PATH)
+
+
+if __name__ == '__main__':
+    print(query_db("select * from prices"))
