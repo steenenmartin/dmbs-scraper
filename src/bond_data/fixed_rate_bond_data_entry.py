87aa1d519e133a445924e7a1a20fb557092788dc
oskro <oskro@orsted.com>
2022-09-19 10:05:11 +0200

    change column names to new format

--- src/bond_data/fixed_rate_bond_data_entry.py
+++ src/bond_data/fixed_rate_bond_data_entry.py
@@ -2,10 +2,10 @@ from ..credit_insitute import CreditInstitute
 
 
 class FixedRateBondDataEntry:
-    def __init__(self, institute: CreditInstitute, years_to_maturity: int, spot_price: float, maximum_years_without_repayment: float, coupon_rate: float, isin: str):
+    def __init__(self, institute: CreditInstitute, years_to_maturity: int, spot_price: float, max_interest_only_period: float, coupon_rate: float, isin: str):
         self.institute = institute
         self.years_to_maturity = years_to_maturity
         self.spot_price = spot_price
-        self.maximum_years_without_repayment = maximum_years_without_repayment
+        self.max_interest_only_period = max_interest_only_period
         self.coupon_rate = coupon_rate
         self.isin = isin
