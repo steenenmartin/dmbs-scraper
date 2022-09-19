80f32885a62dbf33219b29d2c3207681b407e4f1
oskro <oskro@orsted.com>
2022-09-19 15:16:38 +0200

    added filters

--- src/credit_institute_scraper/dashapp/callbacks.py
+++ src/credit_institute_scraper/dashapp/callbacks.py
@@ -16,29 +16,48 @@ def render_page_content(pathname):
     return page_not_found.page_not_found(pathname)
 
 
-@app.callback(Output("daily_plot", "figure"), [Input("select_institute_daily_plot", "value")])
-def update_daily_plot(institute):
+@app.callback(Output("daily_plot", "figure"), [Input("select_institute_daily_plot", "value"),
+                                               Input("select_coupon_daily_plot", "value"),
+                                               Input("select_ytm_daily_plot", "value"),
+                                               Input("select_max_io_daily_plot", "value")])
+def update_daily_plot(institute, coupon_rate, years_to_maturity, max_interest_only_period):
     date = dt.date(2022, 9, 16)
-    years_to_maturity = 30
-    max_interest_only_period = 0
-    df = query_db(f"""
-select * from prices 
-where date(timestamp) = :date 
-and institute = :institute
-and years_to_maturity = :years_to_maturity
-and max_interest_only_period = :max_interest_only_period""", params=locals())
+    sql = f"select * from prices where date(timestamp) = :date"
+
+    filters = []
+    if institute is not None:
+        sql += " and institute = :institute"
+    else:
+        filters.append('institute')
+    if coupon_rate is not None:
+        sql += " and coupon_rate = :coupon_rate"
+    else:
+        filters.append('coupon_rate')
+    if years_to_maturity is not None:
+        sql += " and years_to_maturity = :years_to_maturity"
+    else:
+        filters.append('years_to_maturity')
+
+    if max_interest_only_period is not None:
+        sql += " and max_interest_only_period = :max_interest_only_period"
+    else:
+        filters.append('max_interest_only_period')
+    df = query_db(sql, params=locals())
 
     scatters = []
-    for cr in sorted(df['coupon_rate'].unique()):
-        tmp_df = df[df['coupon_rate'] == cr]
+    groups = df.groupby(filters) if filters else [('', df)]
+    for g, tmp_df in groups:
+        g = g if isinstance(g, (list, tuple)) else [g]
         scatters.append(go.Scatter(x=tmp_df['timestamp'],
                                    y=tmp_df['spot_price'],
                                    line=dict(width=3),
-                                   name=cr,
-                                   line_shape='hv'
+                                   name='<br>'.join(f'{f}: {v}' for f, v in zip(filters, g)),
+                                   line_shape='hv',
+                                   showlegend=True
                                    ))
     fig = go.Figure(scatters)
-    fig.update_layout(title=f'Years to maturity: {years_to_maturity}, max interest only period: {max_interest_only_period}, institute: {institute}, date={date.isoformat()}',
+    fig.update_layout(title=f'Daily spot prices',
                       xaxis_title='Timestamp',
-                      yaxis_title='Spot price')
+                      yaxis_title='Spot price',
+                      )
     return fig
