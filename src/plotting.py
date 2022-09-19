71846f5aa56985dcc6c2a8c5e0b23e715da80461
oskro <oskro@orsted.com>
2022-09-19 10:41:30 +0200

    change col names

--- src/plotting.py
+++ src/plotting.py
@@ -13,10 +13,10 @@ def create_single_day_plot_per_institute(date):
     day_path = f"./plots/{date.strftime('%Y%m%d')}"
 
     kurs_dfs = query_db("select * from prices")
-    kurs_dfs.set_index(["institute", "years_to_maturity", "maximum_years_without_repayment", "coupon_rate"],
+    kurs_dfs.set_index(["institute", "years_to_maturity", "max_interest_only_period", "coupon_rate"],
                        inplace=True)
-    kurs_dfs['time'] = pd.to_datetime(kurs_dfs['time'])
-    kurs_dfs.sort_values("time")
+    kurs_dfs['timestamp'] = pd.to_datetime(kurs_dfs['timestamp'])
+    kurs_dfs.sort_values('timestamp')
 
     for institute in kurs_dfs.index.levels[0].values:
         indices = [i for i in set(kurs_dfs.index.values) if i[0] == institute and i[1] == 30 and i[2] == 0]
@@ -25,7 +25,7 @@ def create_single_day_plot_per_institute(date):
 
             for index in sorted(indices, reverse=True):
                 group_df = kurs_dfs.loc[index]
-                plt.step(group_df["time"], group_df["spot_price"], where='post', label=f"{index[2]} %")
+                plt.step(group_df['timestamp'], group_df["spot_price"], where='post', label=f"{index[2]} %")
                 plt.title(
                     f"{index[0]}: Løbetid {index[1]} år, {index[2]} års afdragsfrihed ({date.strftime('%Y/%m/%d')})")
 
@@ -49,16 +49,16 @@ def create_single_day_plot_per_institute(date):
 
 def create_multi_day_plot():
     kurs_dfs = query_db("select * from prices")
-    kurs_dfs.set_index(["years_to_maturity", "maximum_years_without_repayment", "coupon_rate"], inplace=True)
-    kurs_dfs['time'] = pd.to_datetime(kurs_dfs['time'])
-    kurs_dfs.sort_values("time")
+    kurs_dfs.set_index(["years_to_maturity", "max_interest_only_period", "coupon_rate"], inplace=True)
+    kurs_dfs['timestamp'] = pd.to_datetime(kurs_dfs['timestamp'])
+    kurs_dfs.sort_values('timestamp')
     indices = [i for i in set(kurs_dfs.index.values) if i[0] == 30 and i[1] == 0]
     with sns.color_palette("RdYlGn", n_colors=len(indices)):
         plt.figure(figsize=(20, 15))
 
         for index in sorted(indices, reverse=True):
             group_df = kurs_dfs.loc[index]
-            plt.plot(group_df["time"], group_df["spot_price"], label=f"{index[2]} %")
+            plt.plot(group_df['timestamp'], group_df["spot_price"], label=f"{index[2]} %")
             plt.title(f"Løbetid {index[0]} år, {index[1]} års afdragsfrihed")
 
         ax = plt.gca()
