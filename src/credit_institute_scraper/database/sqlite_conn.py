534786c1f282bc9915353afcb879e564fe86b209
oskro <oskro@orsted.com>
2022-09-20 17:05:08 +0200

    server changes

--- src/credit_institute_scraper/database/sqlite_conn.py
+++ src/credit_institute_scraper/database/sqlite_conn.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlite3
-DATABASE_PATH = f"{__file__}/../../../../database.db"
+import os
+DATABASE_PATH = os.path.abspath(f"{__file__}/../../../../database.db")
 print(DATABASE_PATH)
 
 
@@ -11,19 +12,4 @@ def query_db(sql: str, params: dict = None) -> pd.DataFrame:
 
 
 def client_factory():
-    print(DATABASE_PATH)
-    logging.info(DATABASE_PATH)
     return sqlite3.connect(DATABASE_PATH)
-
-
-if __name__ == '__main__':
-    print(__file__)
-    import logging
-    import credit_institute_scraper
-    import datetime as dt
-    logging.info("test")
-    # df = query_db("select * from prices where to_date(timestamp, 'yyyy-mm-dd') >= :stamp", params={'stamp': dt.datetime(2022, 9, 16)})
-    df = query_db("select * from prices where date(timestamp) = :stamp", params={'stamp': dt.date(2022, 9, 16)})
-    df[:50].to_clipboard()
-    print(df)
-
