923aca3fd4b95e381466b3aa5eba5e225f93657b
oskro <oskro@orsted.com>
2022-09-20 16:11:58 +0200

    relative imports

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from credit_institute_scraper.dashapp.dash_app import dash_app as app
-from credit_institute_scraper.dashapp import layout, callbacks
+from src.credit_institute_scraper.dashapp.dash_app import dash_app as app
+from src.credit_institute_scraper.dashapp import layout, callbacks
 
 app.layout = layout.layout
 server = app.server
