d79c23c21d9fa87f419384d31f2208f2fc806d76
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-09-22 19:11:16 +0200

    Try fix.

--- src/credit_institute_scraper/database/postgres_conn.py
+++ src/credit_institute_scraper/database/postgres_conn.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 import os
 
 DATABASE_PATH = os.environ.get('DATABASE_URL')
+if DATABASE_PATH and DATABASE_PATH.startswith("postgres://"):
+    DATABASE_PATH = DATABASE_PATH.replace("postgres://", "postgresql://", 1)  # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
 
 
 def query_db(sql: str, params: dict = None, cast_date_col=None) -> pd.DataFrame:
