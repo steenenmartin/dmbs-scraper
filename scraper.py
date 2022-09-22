f06bde0bc5f6781ec4a31fcb2b92fb891a317f87
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-09-22 18:56:11 +0200

    Added src to import.

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from credit_institute_scraper.scrapers.run_scraper import scrape
+from src.credit_institute_scraper.scrapers.run_scraper import scrape
 from src.credit_institute_scraper.database import postgres_conn
 from apscheduler.schedulers.blocking import BlockingScheduler
 
