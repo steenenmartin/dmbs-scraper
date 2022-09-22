6b8573af9beaff8b8a9387ce96076d8e39c1e78c
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-09-22 15:44:18 +0200

    Refac for heroku scheduled worker and database.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:server
+scrape: python scraper.py
--- src/credit_institute_scraper/result_handlers/database_result_handler.py
+++ src/credit_institute_scraper/result_handlers/database_result_handler.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-from ..database.sqlite_conn import query_db, client_factory
 from ..result_handlers.result_handler import ResultHandler
 
 
 class DatabaseResultHandler(ResultHandler):
-    def __init__(self, table_name, *args, **kwargs):
+    def __init__(self, database_conn, table_name, *args, **kwargs):
+        self._database_conn = database_conn
         self._table_name = table_name
         super().__init__(*args,  **kwargs)
 
     def export_result(self, result_df: pd.DataFrame) -> None:
-        with client_factory() as conn:
+        with self.database_conn.client_factory() as conn:
             result_df.to_sql(name=self.table_name, con=conn, if_exists='append', index=False)
 
     def result_exists(self) -> bool:
-        result = query_db(f"select * from {self.table_name} where timestamp = '{self.scrape_time}'")
+        result = self.database_conn.query_db(f"select * from {self.table_name} where timestamp = '{self.scrape_time}'")
         return not result.empty
 
     @property
@@ -24,4 +24,8 @@ class DatabaseResultHandler(ResultHandler):
     @property
     def table_name(self):
         return self._table_name
+
+    @property
+    def database_conn(self):
+        return self._database_conn
 
