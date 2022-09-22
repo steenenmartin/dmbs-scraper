8c5843e6fad81ce658f3a42a79a1686dfb485023
oskro <oskro@orsted.com>
2022-09-22 16:17:35 +0200

    local scraper

--- src/credit_institute_scraper/local_scraper.py
+++ src/credit_institute_scraper/local_scraper.py
@@ -1,13 +1,13 @@
 from credit_institute_scraper.scrapers.run_scraper import scrape
-from src.credit_institute_scraper.database import postgres_conn
+from src.credit_institute_scraper.database import sqlite_conn
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour="7-18", minute="*/1")
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour="7-18", minute="*/5")
 def scheduled_job():
-    scrape(postgres_conn)
+    scrape(sqlite_conn)
 
 
 sched.start()
