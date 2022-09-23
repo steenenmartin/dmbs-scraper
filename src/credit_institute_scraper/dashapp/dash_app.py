83b8d0c09a88829f289d3c368c2a11068af12c67
oskro <oskro@orsted.com>
2022-09-23 14:44:27 +0200

    add google analytics tag

--- src/credit_institute_scraper/dashapp/dash_app.py
+++ src/credit_institute_scraper/dashapp/dash_app.py
@@ -7,3 +7,35 @@ dash_app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
 )
 dash_app.title = 'Bond stats'
+
+
+dash_app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V3RXS9EHBM"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-V3RXS9EHBM');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        <div>My Custom header</div>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+        <div>My Custom footer</div>
+    </body>
+</html>
+"""
