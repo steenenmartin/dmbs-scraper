cefdc026c00ac450da5136c0e050e7e01825638c
oskro <oskro@orsted.com>
2022-09-23 14:44:17 +0200

    typing updates

--- src/credit_institute_scraper/bond_data/fixed_rate_bond_data.py
+++ src/credit_institute_scraper/bond_data/fixed_rate_bond_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime as dt
 from .fixed_rate_bond_data_entry import FixedRateBondDataEntry
 
 
@@ -6,8 +7,9 @@ class FixedRateBondData:
     def __init__(self, fixed_rate_bond_data_entries: list[FixedRateBondDataEntry]):
         self.fixed_rate_bond_data_entries = fixed_rate_bond_data_entries
 
-    def to_pandas_data_frame(self, scrape_time):
-        df = pd.DataFrame.from_dict([x.__dict__ for x in self.fixed_rate_bond_data_entries])
-        df.insert(0, "timestamp", scrape_time)
+    def to_pandas_data_frame(self, scrape_time: dt.datetime = None) -> pd.DataFrame:
+        df = pd.DataFrame(x.__dict__ for x in self.fixed_rate_bond_data_entries)
+        if scrape_time is not None:
+            df.insert(0, "timestamp", scrape_time)
 
         return df
--- src/credit_institute_scraper/bond_data/fixed_rate_bond_data_entry.py
+++ src/credit_institute_scraper/bond_data/fixed_rate_bond_data_entry.py
@@ -1,8 +1,11 @@
-from ..enums.credit_insitute import CreditInstitute
-
-
 class FixedRateBondDataEntry:
-    def __init__(self, institute: CreditInstitute, years_to_maturity: int, spot_price: float, max_interest_only_period: float, coupon_rate: float, isin: str):
+    def __init__(self,
+                 institute: str,
+                 years_to_maturity: int,
+                 spot_price: float,
+                 max_interest_only_period: float,
+                 coupon_rate: float,
+                 isin: str):
         self.institute = institute
         self.isin = isin
         self.years_to_maturity = years_to_maturity
