99e68a86d9ec0d32efdcdb949a1e12536d6ee694
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-09-24 23:03:14 +0200

    Added missing historical_plots.py

---
+++ src/credit_institute_scraper/dashapp/pages/historical_plots.py
@@ -0,0 +1,86 @@
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from .. import styles
+
+
+def historical_plot_page():
+    return dbc.Container([
+        dbc.Card(
+            [
+                dbc.Row(
+                    [
+                        html.H4(f'Historic prices', className='header__graph'),
+                        dbc.Col(
+                            [
+                                dbc.Label('Institute'),
+                                dbc.Card(
+                                    [
+                                        dcc.Dropdown(id='select_institute_historical_plot',
+                                                     options=[],
+                                                     multi=True,
+                                                     searchable=False,
+                                                     **styles.DROPDOWN_STYLE)
+                                    ]
+                                ),
+                                html.Br(),
+                                dbc.Label('Coupon'),
+                                dbc.Card(
+                                    [
+                                        dcc.Dropdown(id='select_coupon_historical_plot',
+                                                     options=[],
+                                                     multi=True,
+                                                     searchable=False,
+                                                     **styles.DROPDOWN_STYLE)
+                                    ]
+                                ),
+                                html.Br(),
+                                dbc.Label('Years to maturity'),
+                                dbc.Card(
+                                    [
+                                        dcc.Dropdown(id='select_ytm_historical_plot',
+                                                     options=[],
+                                                     multi=True,
+                                                     searchable=False,
+                                                     **styles.DROPDOWN_STYLE)
+                                    ]
+                                ),
+                                html.Br(),
+                                dbc.Label('Max interest-only period'),
+                                dbc.Card(
+                                    [
+                                        dcc.Dropdown(id='select_max_io_historical_plot',
+                                                     options=[],
+                                                     multi=True,
+                                                     searchable=False,
+                                                     **styles.DROPDOWN_STYLE)
+                                    ]
+                                ),
+                                html.Br(),
+                                dbc.Label("Isin"),
+                                dbc.Card(
+                                    [
+                                        dcc.Dropdown(id='select_isin_historical_plot',
+                                                     options=[],
+                                                     multi=True,
+                                                     searchable=False,
+                                                     **styles.DROPDOWN_STYLE)
+                                    ]
+                                ),
+                            ],
+                            width={"size": 2}
+                        ),
+                        dbc.Col(
+                            dcc.Graph(id='historical_plot',
+                                      figure=dict(
+                                          layout=styles.HISTORICAL_GRAPH_STYLE
+                                      ),
+                                      style={'height': '95vh'}),
+                            width={"size": 10}
+                        ),
+                    ],
+                    **styles.ROW_STYLE
+                ),
+            ], className='graph__container'
+        ),
+    ],
+)
