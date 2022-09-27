56d0d487bc87629a65f74cc97aa8218801e075ab
oskro <oskro@orsted.com>
2022-09-27 09:08:38 +0200

    better logging

--- src/credit_institute_scraper/utils/date_helper.py
+++ src/credit_institute_scraper/utils/date_helper.py
@@ -1,21 +1,26 @@
 import datetime as dt
 from .server_helper import is_heroku_server
+import logging
 
 
 def get_active_date():
-    if not is_heroku_server():
+    if is_heroku_server():
         return dt.date(2022, 9, 21)
 
     date = dt.date.today()
     weekday = dt.date.today().weekday()
+    reduce_days = 0
     if weekday == 6:
-        date -= dt.timedelta(days=2)
+        reduce_days = 2
     elif weekday == 5:
-        date -= dt.timedelta(days=1)
+        reduce_days = 1
     elif dt.datetime.utcnow().hour < 7:
         if weekday == 0:
-            date -= dt.timedelta(days=3)
+            reduce_days = 3
         else:
-            date -= dt.timedelta(days=1)
+            reduce_days = 1
+
+    logging.info(f'Date: {date.isoformat()}, reduced days: {reduce_days}, timestamp: {dt.datetime.utcnow().isoformat()}')
+    date -= dt.timedelta(days=reduce_days)
 
     return date
