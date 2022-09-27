96b6000c6315f33cdd98fda4c3b0060c1e7bcb04
oskro <oskro@orsted.com>
2022-09-27 08:23:39 +0200

    local postgres connection

--- src/credit_institute_scraper/database/postgres_conn.py
+++ src/credit_institute_scraper/database/postgres_conn.py
@@ -2,11 +2,8 @@ import pandas as pd
 import sqlalchemy
 import os
 import logging
-
-DATABASE_PATH = os.environ.get('HEROKU_POSTGRESQL_BRONZE_URL')
-if DATABASE_PATH and DATABASE_PATH.startswith("postgres://"):
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    DATABASE_PATH = DATABASE_PATH.replace("postgres://", "postgresql://", 1)
+import json
+from ..utils.server_helper import is_heroku_server
 
 
 def query_db(sql: str, params: dict = None, cast_date_col=None) -> pd.DataFrame:
@@ -25,5 +22,30 @@ def query_db(sql: str, params: dict = None, cast_date_col=None) -> pd.DataFrame:
 
 
 def client_factory():
-    return sqlalchemy.create_engine(DATABASE_PATH)
+    return sqlalchemy.create_engine(connection_string())
 
+
+def connection_string():
+    if is_heroku_server():
+        database_path = os.environ.get('HEROKU_POSTGRESQL_BRONZE_URL')
+        if database_path and database_path.startswith("postgres://"):
+            # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+            database_path = database_path.replace("postgres://", "postgresql://", 1)
+    else:
+        with open('credentials.json') as fo:
+            crd = json.load(fo)
+        database_path = f'postgresql://{crd["user"]}:{crd["password"]}@{crd["host"]}:{crd["port"]}/{crd["database"]}'
+    return database_path
+
+
+def execute_statements(statements: list):
+    engine = client_factory()
+    conn = engine.connect()
+    trans = conn.begin()
+    try:
+        for statement in statements:
+            conn.execute(statement)
+        trans.commit()
+    except Exception as e:
+        logging.error(e)
+        trans.rollback()
