9a42fe3047ffcab07695a7f5c092a4f77b653c9a
oskro <oskro@orsted.com>
2022-09-28 10:04:55 +0200

    revert server

--- src/credit_institute_scraper/dashapp/local_server.py
+++ src/credit_institute_scraper/dashapp/local_server.py
@@ -1,14 +1,7 @@
 from credit_institute_scraper.dashapp.dash_app import dash_app as app
 from credit_institute_scraper.dashapp import layout, callbacks
-from flask import redirect
+
 
 app.layout = layout.layout
 server = app.server
-
-
-@server.before_request
-def hello():
-    return redirect('/')
-
-
 server.run(port=8080)
