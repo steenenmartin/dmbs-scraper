c54bbb2bca3b264800cf212edb6d1cd01f1b3afe
oskro <oskro@orsted.com>
2022-09-28 10:44:40 +0200

    suppress exceptions

--- src/credit_institute_scraper/dashapp/dash_app.py
+++ src/credit_institute_scraper/dashapp/dash_app.py
@@ -6,6 +6,7 @@ dash_app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
     external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True,
 )
 dash_app.title = 'Bond stats'
 
