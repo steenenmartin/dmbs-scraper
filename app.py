f897827a01df304e61e8d04b071354b80f13029f
oskro <oskro@orsted.com>
2022-09-28 10:48:05 +0200

    suppress exceptions

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from src.credit_institute_scraper.dashapp.dash_app import dash_app as app
 from src.credit_institute_scraper.dashapp import layout, callbacks
+import logging
 
 
 app.layout = layout.layout
 server = app.server
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 
