b0767260c45631951884d56e6d8dbfa30797331f
oskro <oskro@orsted.com>
2022-09-28 08:51:52 +0200

    test fix for webpage

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from src.credit_institute_scraper.dashapp.dash_app import dash_app as app
 from src.credit_institute_scraper.dashapp import layout, callbacks
+from flask_caching import Cache
 
 app.layout = layout.layout
 server = app.server
+cache = Cache(app.server, config={'CACHE_TYPE': 'simple'})
+cache.clear()
