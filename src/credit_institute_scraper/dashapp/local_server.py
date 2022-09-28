f585bfcebff0e67332fde177f453e5309cd7a276
oskro <oskro@orsted.com>
2022-09-28 08:51:52 +0200

    test fix for webpage

--- src/credit_institute_scraper/dashapp/local_server.py
+++ src/credit_institute_scraper/dashapp/local_server.py
@@ -1,6 +1,9 @@
 from credit_institute_scraper.dashapp.dash_app import dash_app as app
 from credit_institute_scraper.dashapp import layout, callbacks
+from flask_caching import Cache
 
 app.layout = layout.layout
 server = app.server
+cache = Cache(app.server, config={'CACHE_TYPE': 'simple'})
+cache.clear()
 server.run(port=8080)
