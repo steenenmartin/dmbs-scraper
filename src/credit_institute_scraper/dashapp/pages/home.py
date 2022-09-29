f90cfa7a06ca431b7a863475f7d256cb11d47236
oskro <oskro@orsted.com>
2022-09-29 15:24:12 +0200

    fix table view on mobile

--- src/credit_institute_scraper/dashapp/pages/home.py
+++ src/credit_institute_scraper/dashapp/pages/home.py
@@ -18,6 +18,6 @@ def home_page():
                                        html.Small(" e.g. you could filter the Coupon column by writing "),
                                        html.Code('>0.5', className='code-container')]),
                     html.Div(id='data_table_div', style={'margin-top': '1rem'})
-                ], style={'width': '60vh'}),
+                ], style={'width': '55rem'}),
         ]
     )
