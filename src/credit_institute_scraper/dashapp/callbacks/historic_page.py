94bee237cf0fa1e8d2dcf150b7a58d56ec6d13b6
oskro <oskro@orsted.com>
2022-09-30 15:35:31 +0200

    range break example

--- src/credit_institute_scraper/dashapp/callbacks/historic_page.py
+++ src/credit_institute_scraper/dashapp/callbacks/historic_page.py
@@ -108,3 +108,20 @@ def update_historical_plot(active_cell, df, isin_data):
     logging.info(f'Updated historical plot figure with isin = {isin}')
 
     return fig
+
+
+"""
+import plotly.express as px
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = px.scatter(df, x='Date', y='AAPL.High', range_x=['2015-12-01', '2016-01-15'],
+                 title="Hide Gaps with rangebreaks")
+fig.update_xaxes(
+    rangebreaks=[
+        dict(bounds=["sat", "mon"]), #hide weekends
+        dict(values=["2015-12-25", "2016-01-01"])  # hide Christmas and New Year's
+    ]
+)
+fig.show()
+"""
