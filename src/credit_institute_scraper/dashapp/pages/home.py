cbe8414a57b8239b5a4042891faee3cffbd81fb5
oskro <oskro@orsted.com>
2022-10-04 14:51:16 +0200

    row/col in home page

--- src/credit_institute_scraper/dashapp/pages/home.py
+++ src/credit_institute_scraper/dashapp/pages/home.py
@@ -7,16 +7,25 @@ def home_page():
         [
             html.Div(
                 [
-                    html.H3("Cumulative daily changes", className='header__card'),
-                    html.Div(children=[html.Small("To filter numerical columns, prepend with either "),
-                                       html.Code('=', className='code-container'),
-                                       html.Small(', '),
-                                       html.Code('>', className='code-container'),
-                                       html.Small(' or '),
-                                       html.Code('<', className='code-container'),
-                                       html.Small(" - e.g. you could filter the Coupon column by writing "),
-                                       html.Code('>0.5', className='code-container')]),
-                    html.Div(id='data_table_div', style={'margin-top': '1rem'})
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                [
+                                    html.H3("Cumulative daily changes", className='header__card'),
+                                    html.Div(children=[html.Small("To filter numerical columns, prepend with either "),
+                                                       html.Code('=', className='code-container'),
+                                                       html.Small(', '),
+                                                       html.Code('>', className='code-container'),
+                                                       html.Small(' or '),
+                                                       html.Code('<', className='code-container'),
+                                                       html.Small(
+                                                           " - e.g. you could filter the Coupon column by writing "),
+                                                       html.Code('>0.5', className='code-container')]),
+                                    html.Div(id='data_table_div', style={'margin-top': '1rem'})
+                                ]
+                            )
+                        ]
+                    )
                 ], style={'width': '55rem'}),
         ]
     )
