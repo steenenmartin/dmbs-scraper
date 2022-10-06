a0d1254eca3ae5543da500294864b0315e1d6216
oskro <oskro@orsted.com>
2022-10-06 10:34:07 +0200

    custom page titles for each subpage

--- src/credit_institute_scraper/dashapp/dash_app.py
+++ src/credit_institute_scraper/dashapp/dash_app.py
@@ -1,5 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash import Input, Output
 from ..utils.server_helper import is_heroku_server
 
 dash_app = dash.Dash(
@@ -8,7 +9,46 @@ dash_app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True,
 )
-dash_app.title = 'Bond stats'
+
+# dash_app.title = 'Bond stats'
+
+
+# dash_app.clientside_callback(
+#     """
+#     function(url) {
+#         function getQueryVariable(variable) {
+#             var query = window.location.search.substring(1);
+#             var vars = query.split('&');
+#             for (var i = 0; i < vars.length; i++) {
+#                 var pair = vars[i].split('=');
+#                 if (decodeURIComponent(pair[0]) == variable) {
+#                     return decodeURIComponent(pair[1]);
+#                 }
+#             }
+#             console.log('Query variable %s not found', variable);
+#         }
+#             document.title = 'test'
+#     }
+#     """,
+#     Output('page-title-output', 'children'),
+#     Input('url', 'search')
+# )
+
+dash_app.clientside_callback(
+    """
+    function(tab_value) {
+        let str = tab_value.substring(1);
+        if (str.length === 0) {
+            document.title = 'Bond stats'
+        }
+        else {
+            document.title = str[0].toUpperCase() + str.substring(1) + ' | Bond stats'
+        }
+    }
+    """,
+    Output('page-title-output', 'children'),
+    Input('url', 'pathname')
+)
 
 if is_heroku_server():
     dash_app.index_string = """
