014d1c9ebdbf3161e9097bf79fbfd5fdf52f27d5
oskro <oskro@orsted.com>
2022-10-06 12:02:13 +0200

    add active date to home page

--- src/credit_institute_scraper/dashapp/callbacks/home_cb.py
+++ src/credit_institute_scraper/dashapp/callbacks/home_cb.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from dash import Output, Input, dash_table
+from dash import Output, Input, dash_table, html
 from ..dash_app import dash_app as app
 from .utils import data_bars_diverging, table_type
 
@@ -17,31 +17,39 @@ def load_home_page_table(spot_prices, master_data):
     }
 
     spot_prices = pd.DataFrame(spot_prices)
+    active_date = spot_prices['timestamp'].iloc[-1][:10]
     master_data = pd.DataFrame(master_data)
     spot_prices = pd.merge(spot_prices, master_data, on="isin")
 
-    spot_prices = spot_prices.dropna()\
-        .groupby(['institute', 'coupon_rate', 'years_to_maturity', 'max_interest_only_period', 'isin'])\
-        .agg(Price=('spot_price', lambda x: x.iat[-1]), p_ch=('spot_price', lambda x: x.iat[-1] - x.iat[0]))\
-        .round(2)\
-        .reset_index()\
+    spot_prices = spot_prices.dropna() \
+        .groupby(['institute', 'coupon_rate', 'years_to_maturity', 'max_interest_only_period', 'isin']) \
+        .agg(Price=('spot_price', lambda x: x.iat[-1]), p_ch=('spot_price', lambda x: x.iat[-1] - x.iat[0])) \
+        .round(2) \
+        .reset_index() \
         .rename(columns={'p_ch': 'Δ Price'})
     ascending = True if spot_prices['Δ Price'].mean() < 0 else False
     spot_prices = spot_prices.sort_values(by='Δ Price', ascending=ascending)
-    return dash_table.DataTable(id='home_page_table',
-                                data=spot_prices.to_dict('records'),
-                                sort_action='native',
-                                columns=[{'name': col_name_map.get(i, i), 'id': i, 'type': table_type(spot_prices[i])} for i in spot_prices.columns],
-                                style_data_conditional=(
-                                    data_bars_diverging(spot_prices, 'Δ Price', zero_mid=True)
-                                ),
-                                style_cell={
-                                    'width': '2rem',
-                                    'minWidth': '2rem',
-                                    'maxWidth': '2rem',
-                                    'overflow': 'hidden',
-                                    'textOverflow': 'ellipsis',
-                                },
-                                page_size=20,
-                                filter_action='native',
-                                )
+    return html.Div(
+        [
+            html.Label(active_date, className='table__header'),
+            dash_table.DataTable(
+                id='home_page_table',
+                data=spot_prices.to_dict('records'),
+                sort_action='native',
+                columns=[{'name': col_name_map.get(i, i), 'id': i, 'type': table_type(spot_prices[i])} for i in
+                         spot_prices.columns],
+                style_data_conditional=(
+                    data_bars_diverging(spot_prices, 'Δ Price', zero_mid=True)
+                ),
+                style_cell={
+                    'width': '2rem',
+                    'minWidth': '2rem',
+                    'maxWidth': '2rem',
+                    'overflow': 'hidden',
+                    'textOverflow': 'ellipsis',
+                },
+                page_size=20,
+                filter_action='native',
+            )
+        ]
+    )
