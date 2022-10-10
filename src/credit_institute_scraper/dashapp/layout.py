06430f90272108c8ceaf46ae55db56e516099742
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-10-10 21:15:26 +0200

    Add status indicators to side bar (works, retarded).

--- src/credit_institute_scraper/dashapp/layout.py
+++ src/credit_institute_scraper/dashapp/layout.py
@@ -1,10 +1,41 @@
 from . import styles
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+import dash_daq as daq
+
+import pandas as pd
+from datetime import datetime, timedelta
+
+from ..database import postgres_conn
+from ..enums.credit_insitute import CreditInstitute
+
+
+def load_status_table(institute: CreditInstitute):
+    now = datetime.utcnow()
+    today = now.date()
+
+    master_data = postgres_conn.query_db(f"select * from master_data where institute = '{institute.name}'")
+    isins = "', '".join(set(master_data['isin']))
+    spot_prices = postgres_conn.query_db(f"select * from spot_prices where date(timestamp) = '{today}' and isin in ('{isins}')")
+    spot_prices['timestamp'] = pd.to_datetime(spot_prices['timestamp'])
+
+    def status_color(timestamp):
+        if timestamp is None:
+            return "red"
+        if 15 < now.hour or now.hour < 7:
+            return "grey"
+        elif now - timedelta(minutes=5) > timestamp:
+            return "red"
+        else:
+            return "green"
+
+    latest_institute_data_timestamp = None if spot_prices.empty else spot_prices['timestamp'].iloc[-1]
+    return status_color(latest_institute_data_timestamp)
 
 
 sidebar = html.Div(
     [
+
         html.I(className="app__header",
                style={'background-image': 'url(/assets/favicon.ico)',
                       'height': '16rem',
@@ -21,11 +52,26 @@ sidebar = html.Div(
             vertical=True,
             pills=True,
         ),
+        html.Hr(),
+        html.H5("Status", style={'color': "white"}, className='app__header'),
+        html.Br(),
+        daq.Indicator(
+            label=CreditInstitute.RealKreditDanmark.name,
+            color=load_status_table(CreditInstitute.RealKreditDanmark),
+            style={"color": "white"},
+        ),
+        html.Br(),
+        daq.Indicator(
+            label=CreditInstitute.Jyske.name,
+            color=load_status_table(CreditInstitute.Jyske),
+            style={"color": "white"},
+        ),
     ],
     id='sidebar',
     style=styles.SIDEBAR_STYLE,
     className='sidebar'
 )
+
 
 content = html.Div(id="page-content", style=styles.CONTENT_STYLE)
 layout = html.Div(
