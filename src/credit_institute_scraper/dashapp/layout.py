f8cfed4e5319df7cf8ddac4ebad13e8440053383
oskro <oskro@orsted.com>
2022-10-14 14:45:52 +0200

    fix logo image aspect ratio

--- src/credit_institute_scraper/dashapp/layout.py
+++ src/credit_institute_scraper/dashapp/layout.py
@@ -7,7 +7,7 @@ sidebar = html.Div(
     [
         html.I(className="app__header",
                style={'background-image': 'url(/assets/favicon.ico)',
-                      'height': '16rem',
+                      'padding-top': '100%',
                       'background-size': '100%'}),
         html.H3("Bond stats", className='app__header'),
         html.Hr(),
@@ -29,20 +29,24 @@ sidebar = html.Div(
     className='sidebar'
 )
 
+stores = [
+    dcc.Location(id="url", refresh=False),
+    html.Div(id='page-title-output'),
+    html.Div(id='dummy1', style={'display': 'none'}),
+    html.Div(id='dummy2', style={'display': 'none'}),
+    html.Div(id='date_range_div', style={'display': 'none'}),
+    dcc.Store(id='daily_store', data=None),
+    dcc.Store(id='master_data', data=None),
+    dcc.Store(id='side_click'),
+    dcc.Interval(id='interval-component', interval=60000, n_intervals=0),
+    dcc.Loading(type="default", children=html.Div(id="loading-spinner-output1"), className='spinner'),
+]
+
 content = html.Div(id="page-content", style=styles.CONTENT_STYLE)
 layout = html.Div(
     [
-        dcc.Location(id="url", refresh=False),
-        html.Div(id='page-title-output'),
-        html.Div(id='dummy1', style={'display': 'none'}),
-        html.Div(id='dummy2', style={'display': 'none'}),
-        dcc.Store(id='daily_store', data=None),
-        dcc.Store(id='master_data', data=None),
-        dcc.Interval(id='interval-component', interval=60000, n_intervals=0),
-        html.Div(id='date_range_div', style={'display': 'none'}),
-        dcc.Loading(type="default", children=html.Div(id="loading-spinner-output1"), className='spinner'),
-        dcc.Store(id='side_click'),
-        dbc.Button("Hide", outline=True, id="btn_sidebar", className='sidebar-btn'),
+        *stores,
+        dbc.Button(outline=True, id="btn_sidebar", className='sidebar-btn'),
         sidebar,
         content
      ]
