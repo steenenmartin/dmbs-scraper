7e83653c6decbee32565a48e9c0f38e7bcb35d41
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-10-15 22:27:32 +0200

    Added 2023 trading calendar holidays.

--- src/credit_institute_scraper/utils/date_helper.py
+++ src/credit_institute_scraper/utils/date_helper.py
@@ -25,6 +25,15 @@ def get_active_date():
 def is_holiday(date):
     if date.date() in [
         dt.date(2022, 12, 26),
+        dt.date(2023, 4, 7),
+        dt.date(2023, 4, 10),
+        dt.date(2023, 5, 5),
+        dt.date(2023, 5, 18),
+        dt.date(2023, 5, 19),
+        dt.date(2023, 5, 29),
+        dt.date(2023, 6, 5),
+        dt.date(2023, 12, 25),
+        dt.date(2023, 12, 26),
     ]:
         return True
     return False
