d6d5647c83386e8341db5350445f15db8d15512c
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-10-15 19:11:37 +0200

    Added data corrections in realkredit_danmark_scraper.

--- src/credit_institute_scraper/scrapers/realkredit_danmark_scraper.py
+++ src/credit_institute_scraper/scrapers/realkredit_danmark_scraper.py
@@ -6,17 +6,33 @@ from ..scrapers.scraper import Scraper
 class RealKreditDanmarkScraper(Scraper):
     @Scraper.scraper
     def parse_fixed_rate_bonds(self, data) -> list[FixedRateBondDataEntry]:
-        return [
-            FixedRateBondDataEntry(
+        bonds: list[FixedRateBondDataEntry] = []
+        for product in data:
+            years_to_maturity = int(float(product["termToMaturityYears"]))
+            isin = product["isinCode"]
+
+            if isin == "DK0004618733" and years_to_maturity == 0:
+                years_to_maturity = 30
+
+            max_io_terms = float(product["numberOfTermsWithoutRepayment"])
+            if max_io_terms == 119.0:
+                max_io_terms = 120.0
+
+            max_io_years = max_io_terms * 3.0 / 12.0
+
+            bond = FixedRateBondDataEntry(
                 self.institute.name,
-                int(float(product["termToMaturityYears"])),
+                years_to_maturity,
                 float(product["prices"][0]["price"].replace(",", ".")),
                 float(product["offerprice"]),
-                float(product["numberOfTermsWithoutRepayment"]) * 3.0 / 12.0,
+                max_io_years,
                 float(product["nominelInterestRate"]),
-                product["isinCode"]
-            ) for product in data
-        ]
+                isin
+            )
+
+            bonds.append(bond)
+
+        return bonds
 
     @property
     def url(self) -> str:
