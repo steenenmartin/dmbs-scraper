f23d7d12f34975128ba30a0d8d25fd1f7884a3a7
Martin Steen Andersen <martinsteenandersen@gmail.com>
2023-04-26 09:42:58 +0200

    Fix

--- adhoc/add_closing_table.py
+++ adhoc/add_closing_table.py
@@ -5,4 +5,5 @@ from credit_institute_scraper.result_handlers.database_result_handler import Dat
 
 if __name__ == '__main__':
     closing_prices = postgres_conn.query_db(f"select timestamp, isin, close_price from ohlc_prices")
-    DatabaseResultHandler(postgres_conn, 'closing_prices', datetime.utcnow()).export_result(closing_prices)
+    closing_prices.rename(columns={"close_price": "spot_price"}, inplace=True)
+    DatabaseResultHandler(postgres_conn, 'closing_prices', datetime.utcnow()).export_result(closing_prices, if_exists="replace")
