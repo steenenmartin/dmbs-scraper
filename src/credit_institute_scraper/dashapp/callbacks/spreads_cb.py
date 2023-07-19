e69dfec82a50d7de7b9a87db1447e1ea69261293
oskro <oskro@orsted.com>
2023-07-19 14:13:54 +0200

    working version of rate spreads

--- src/credit_institute_scraper/dashapp/callbacks/spreads_cb.py
+++ src/credit_institute_scraper/dashapp/callbacks/spreads_cb.py
@@ -2,8 +2,10 @@ from dash import Input, Output, State
 from dash.exceptions import PreventUpdate
 from .utils import update_dropdowns, update_search_bar_template
 from ..dash_app import dash_app as app
+from .. import styles
+from colour import Color
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 
 @app.callback([
@@ -19,19 +21,33 @@ def update_dropdowns_spreads_plot(master_data):
     Output('rate_spreads_plot', 'figure'),
     Input('base_isin_spread', 'value'),
     Input('compare_isins_spread', 'value'),
-    State('spot_prices_store', 'data')
+    Input('spot_prices_store', 'data'),
 )
 def update_spreads_plot(base_isin, compare_isins, data):
-    if not base_isin or not compare_isins:
+    if not base_isin or not compare_isins or data is None:
         raise PreventUpdate
     df = pd.DataFrame(data)
-    df = df[df['isin'].isin([base_isin, *compare_isins])]
 
-    fig = px.scatter(
-        df,
-        x='timestamp',
-        y='spot_price',
-    )
+    lines = []
+    colors = Color("darkblue").range_to(Color("#34a1fa"), len(compare_isins))
+    base = df[df['isin'] == base_isin].sort_values(by='timestamp')
+    for gd, c in zip(df[df['isin'].isin(compare_isins)].groupby('isin'), colors):
+        group, dat = gd
+        dat = dat.sort_values(by='timestamp')
+        lines.append(
+            go.Scatter(
+                x=dat['timestamp'],
+                y=dat['spot_price'].values - base['spot_price'].values,
+                line=dict(width=3, shape='hv'),
+                name=group,
+                hovertemplate='Date: %{x}<br>Spread: %{y:.2f}',
+                showlegend=False,
+                marker={'color': c.get_hex()},
+            )
+        )
+    fig = go.Figure(lines)
+    fig.update_layout(**styles.__graph_style(x_axis_title="Date", show_historic=False))
+
     return fig
 
 
