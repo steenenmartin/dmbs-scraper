6e1beb0a8676ad907265647cf662b052cdeffa22
Martin Steen Andersen <martin@aage-boyhus.dk>
2024-11-21 08:39:44 +0100

    Fix duplicate db entries

--- adhoc/fix_duplicate_db_entries.py
+++ adhoc/fix_duplicate_db_entries.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-
+import sqlalchemy
 from credit_institute_scraper.database import postgres_conn
 from credit_institute_scraper.result_handlers.database_result_handler import DatabaseResultHandler
 
 if __name__ == '__main__':
-    spot_prices = postgres_conn.query_db(f"SELECT * FROM spot_prices where timestamp = '2022-10-28 10:30:00'")
-    unique = spot_prices.drop_duplicates()
-    postgres_conn.client_factory().execute("DELETE from spot_prices where timestamp = '2022-10-28 10:30:00'")
-    DatabaseResultHandler(postgres_conn, "spot_prices", datetime.utcnow()).export_result(unique)
+    where = "where spot_price = -1"
+    print(postgres_conn.query_db("select * from closing_prices " + where).head())
+    with postgres_conn.client_factory().connect() as conn:
+        result = conn.execute(sqlalchemy.text("DELETE from spot_prices " + where))
+        conn.commit()
